46bd1eebc7091e8de51631519bc88dcd72e2237a
Dániel Varjaskéri <varjaskerid@gmail.com>
2021-12-20 09:33:18 +0100

    userplay added

--- R_P_S.js
+++ R_P_S.js
@@ -1,5 +1,39 @@
-function computerPlay(){
-    let options = ["Rock","Paper","Scissors"];
+function game(){ 
 
-   return options[  Math.floor(Math.random()*3) ];
+
+function computerPlay(){
+    let options = ["rock","paper","scissors"];
+
+   return options[  /*Math.floor(Math.random()*3)*/0 ];
 }
+
+computerPlay();
+
+
+
+function userPlay(){
+   let input = window.prompt("Your Play?");
+   input.toLowerCase();
+
+}
+
+userPlay();
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
