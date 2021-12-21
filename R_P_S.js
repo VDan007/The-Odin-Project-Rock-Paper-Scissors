b6d906c247f4ea29e20c46b03b5e1b785f62080b
Dániel Varjaskéri <varjaskerid@gmail.com>
2021-12-21 06:11:07 +0100

    1 round complete

--- R_P_S.js
+++ R_P_S.js
@@ -4,24 +4,58 @@ function game(){
 function computerPlay(){
     let options = ["rock","paper","scissors"];
 
-   return options[  /*Math.floor(Math.random()*3)*/0 ];
+   return options[  Math.floor(Math.random()*3) ];
+
 }
 
-computerPlay();
+let computerSelection = computerPlay();
+console.log(computerSelection);
 
 
 
 function userPlay(){
    let input = window.prompt("Your Play?");
-   input.toLowerCase();
-
+   return input.toLowerCase();
+  
 }
 
-userPlay();
+let playerSelection = userPlay();
+console.log(playerSelection);
 
 
 
+if(computerSelection == playerSelection){
+   return "Draw";
+}
+else if (computerSelection == "rock"){
+   if(playerSelection == "scissors"){
+      return "You lose! rock beats scissors";
+   }else if(playerSelection == "paper"){
+      return "You win! paper beats rock";
+   }else if(playerSelection !== "rock" || "paper" || "scissors"){
+      return "No such play, please select from: rock, paper, scissors";
+   }
+}
 
+else if (computerSelection == "paper"){
+   if(playerSelection == "scissors"){
+      return "You win! scissors beats paper";
+   }else if(playerSelection == "rock"){
+      return "You lose! paper beats rock";
+   }else if(playerSelection !== "rock" || "paper" || "scissors"){
+      return "No such play, please select from: rock, paper, scissors";
+   }
+}
+
+else if (computerSelection == "scissors"){
+   if(playerSelection == "rock"){
+      return "You win! rock beats scissors";
+   }else if(playerSelection == "paper"){
+      return "You lose! scissors beats paper";
+   }else if(playerSelection !== "rock" || "paper" || "scissors"){
+      return "No such play, please select from: rock, paper, scissors";
+   }
+}
 
 
 }
