19f91a1f8ef41e5c730fc6c2aa0a12e939ebf200
Dániel Varjaskéri <varjaskerid@gmail.com>
2021-12-30 20:46:16 +0100

    almost done

--- R_P_S.js
+++ R_P_S.js
@@ -1,10 +1,15 @@
-function round(x){ 
+   var playerS = 0;
+   var computerS =0;
+   
+    
 
+function round(x){ 
+   
 
 function computerPlay(){
     let options = ["rock","paper","scissors"];
 
-   return options[  Math.floor(Math.random()*3)   ];
+   return options[ Math.floor(Math.random()*3) ];
 
 }
 
@@ -24,14 +29,21 @@ console.log("Plyer selected:"+ playerSelection);
 
 if(computerSelection == playerSelection){
    outcomeBoard.textContent= "Draw";
+   
    return "Draw";
 }
 else if (computerSelection == "rock"){
    if(playerSelection == "scissors"){
       outcomeBoard.textContent="You lose! rock beats scissors";
+      computerS++;
+      cS.textContent ="Computer Score"+" "+`${computerS}` ;
+      
       return "Computer wins";
    }else if(playerSelection == "paper"){
       outcomeBoard.textContent="You win! paper beats rock";
+      playerS++;
+      pS.textContent ="Player Score:" + " " + `${playerS}` ;
+      
       return "Player wins";
    }else if(playerSelection !== "rock" || "paper" || "scissors"){
       console.log(  "No such play, please select from: rock, paper, scissors");
@@ -41,9 +53,15 @@ else if (computerSelection == "rock"){
 else if (computerSelection == "paper"){
    if(playerSelection == "scissors"){
       outcomeBoard.textContent=  "You win! scissors beats paper";
+      playerS++;
+      pS.textContent ="Player Score:" + " " + `${playerS}` ;
+      
       return "Player wins";
    }else if(playerSelection == "rock"){
       outcomeBoard.textContent= "You lose! paper beats rock";
+      computerS++;
+      cS.textContent ="Computer Score"+" "+`${computerS}` ;
+      
       return "Computer wins";
    }else if(playerSelection !== "rock" || "paper" || "scissors"){
       console.log( "No such play, please select from: rock, paper, scissors");
@@ -53,9 +71,15 @@ else if (computerSelection == "paper"){
 else if (computerSelection == "scissors"){
    if(playerSelection == "rock"){
       outcomeBoard.textContent="You win! rock beats scissors";
+      playerS++;
+      pS.textContent ="Player Score:" + " " + `${playerS}` ;
+      
       return "Player wins";
    }else if(playerSelection == "paper"){
       outcomeBoard.textContent= "You lose! scissors beats paper";
+      computerS++;
+      cS.textContent ="Computer Score"+" "+`${computerS}` ;
+      
       return "Computer wins";
    }else if(playerSelection !== "rock" || "paper" || "scissors"){
       console.log( "No such play, please select from: rock, paper, scissors");
@@ -97,3 +121,19 @@ const computerSelectionBoard = document.querySelector(".computerSelectionBoard")
 const compSelection = document.createElement("h3");
 
 const outcomeBoard = document.querySelector(".outcomeBoard");
+
+const pS = document.querySelector("#playerScore");
+pS.textContent ="Player Score:" + " " + `${playerS}` ;
+const cS =document.querySelector("#computerScore");
+cS.textContent ="Computer Score"+" "+`${computerS}` ;
+
+const end = document.querySelector("#end");
+const endh1 = document.createElement("h1");
+end.append(endh1);
+
+if(playerS == 0){
+   endh1.textContent ="Hurray";
+}
+else if (playerS == 1){
+   endh1.textContent ="Burray";
+}
